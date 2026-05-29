be73ac509c4a00a992fdef278140a2325a9f6375
cws0811 <2553s2610635@g.cnees.kr>
2026-05-29 09:17:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
 import streamlit as st
-st.title('나는 짱이다')
-st.write('배고프다')
+import random
+
+# 저녁 메뉴 리스트
+menu_list = [
+    "치킨",
+    "피자",
+    "햄버거",
+    "떡볶이",
+    "삼겹살",
+    "초밥",
+    "파스타",
+    "김치찌개",
+    "돈까스",
+    "마라탕"
+]
+
+# 제목
+st.title("🍽 오늘 저녁 추천 앱")
+
+st.write("버튼을 누르면 오늘 저녁 메뉴를 추천해줘요!")
+
+# 버튼
+if st.button("추천 받기"):
+    menu = random.choice(menu_list)
+    st.success(f"오늘 저녁은 👉 {menu}")
